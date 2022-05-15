24f9354ef0c1388c89f9d01252fdc882b524fc5a
ariyan1985 <nassirariyan85@gmail.com>
2022-05-15 08:01:57 -0700

    The basic of the Car is ready, Working on Advance Parts

---
+++ src/Class02/BasicFeatures.java
@@ -0,0 +1,12 @@
+package Class02;
+
+public class BasicFeatures {
+    String engineCapacity;
+    String Color;
+    String doors;
+    String Headlights;
+    String regNo;
+
+
+
+}
---
+++ src/Class02/Main.java
@@ -0,0 +1,10 @@
+package Class02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BasicFeatures car=new BasicFeatures();
+        
+
+    }
+}
